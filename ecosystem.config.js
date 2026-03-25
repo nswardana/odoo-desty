b73952bf9c2b87cf11c68dbc9a04c5394d38cdc5
Nanang Setya Wardana <nswardana@gmail.com>
2026-03-25 18:40:22 +0700

    perbaikan exosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,20 @@ module.exports = {
     {
       name: "sby-desty-api",
       script: "server.js",
-      instances: "max", // gunakan semua core CPU
+      cwd: "/home/odoo/odoo-desty-sby",
+      instances: "max",
       exec_mode: "cluster",
       watch: false,
+
+      autorestart: true,
+      max_restarts: 10,
+      restart_delay: 5000,
+      max_memory_restart: "300M",
+
+      out_file: "./logs/api-out.log",
+      error_file: "./logs/api-error.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+
       env: {
         NODE_ENV: "production"
       }
@@ -13,20 +24,23 @@ module.exports = {
     {
       name: "sby-desty-worker",
       script: "worker.js",
-      instances: 1, // worker biasanya 1 dulu
+      cwd: "/home/odoo/odoo-desty-sby",
+      instances: 1,
       exec_mode: "fork",
       watch: false,
+
+      autorestart: true,
+      max_restarts: 10,
+      restart_delay: 5000,
+      max_memory_restart: "300M",
+
+      out_file: "./logs/worker-out.log",
+      error_file: "./logs/worker-error.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+
       env: {
         NODE_ENV: "production"
       }
     }
-
-    // OPTIONAL kalau mau multiple worker pakai script custom
-    // {
-    //   name: "desty-workers",
-    //   script: "scripts/startWorkers.js",
-    //   instances: 1,
-    //   exec_mode: "fork"
-    // }
   ]
 };
